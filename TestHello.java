111a9840a8b814e9c72fb26a1cfd4995f87e2517
Saikamala <devikatodkari@gmail.com>
2025-08-01 18:03:08 +0530

    Made some changes

--- TestHello.java
+++
@@ -1,9 +0,0 @@
-public class TestHello {
-    public static void main(String[] args) {
-        String name = "Saikamala";
-        int Age = 30;
-
-        System.out.println("My name is " + name + " and my age is " + Age);
-    }
-    
-}
